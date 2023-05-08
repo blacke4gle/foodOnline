77e32904ec50ddc754cbe677c765db7313a7d49d
blacke4gle <ahmetdemirr0@gmail.com>
2023-05-08 15:26:00 +0300

    restaurant profile + google geocoding

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'accounts.context_processors.get_vendor',
+                'accounts.context_processors.get_google_api',
             ],
         },
     },
@@ -154,3 +155,5 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 DEFAULT_FROM_EMAIL = 'foodOnline Marketplace <foodonlinetestt@gmail.com>'
+
+GOOGLE_API_KEY = 'AIzaSyDH5M_w-A5MfeOPAi4Bcy-fbUgFtIpnwek'
---
+++ foodOnline_main/static/js/custom.js
@@ -0,0 +1,72 @@
+let autocomplete;
+
+function initAutoComplete() {
+  autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById("id_address"),
+    {
+      types: ["geocode", "establishment"],
+      //default in this app is "IN" - add your country code
+      componentRestrictions: { country: ["tr"] },
+    }
+  );
+  // function to specify what should happen when the prediction is clicked
+  autocomplete.addListener("place_changed", onPlaceChanged);
+}
+
+function onPlaceChanged() {
+  let place = autocomplete.getPlace();
+
+  // User did not select the prediction. Reset the input field or alert()
+  if (!place.geometry) {
+    document.getElementById("id_address").placeholder = "Start typing...";
+  } else {
+    // console.log("place name=>", place.name);
+  }
+  // get the address components and assign them to the fields
+  let geocoder = new google.maps.Geocoder();
+  let address = document.getElementById("id_address").value;
+
+  geocoder.geocode({ address: address }, function (results, status) {
+    // console.log("result=>", results);
+    // console.log("status=>", status);
+
+    if (status == google.maps.GeocoderStatus.OK) {
+      let latitude = results[0].geometry.location.lat();
+      let longitude = results[0].geometry.location.lng();
+
+      //   console.log("latitude=> ", latitude);
+      //   console.log("longitude=> ", longitude);
+      $("#id_latitude").val(latitude);
+      $("#id_longitude").val(longitude);
+
+      $("#id_address").val(address);
+    }
+  });
+
+  // loop through the address components and assign other address data
+  // console.log(place.address_components);
+  for (let i = 0; i < place.address_components.length; i++) {
+    for (let j = 0; j < place.address_components[i].types.length; j++) {
+      // get country
+      if (place.address_components[i].types[j] == "country") {
+        $("#id_country").val(place.address_components[i].long_name);
+      }
+      // get state
+      if (
+        place.address_components[i].types[j] == "administrative_area_level_1"
+      ) {
+        $("#id_state").val(place.address_components[i].long_name);
+      }
+      // get city
+      if (place.address_components[i].types[j] == "locality") {
+        $("#id_city").val(place.address_components[i].long_name);
+      }
+      // get pincode
+      if (place.address_components[i].types[j] == "postal_code") {
+        $("#id_pin_code").val(place.address_components[i].long_name);
+      } else {
+        $("#id_pin_code").val("");
+      }
+    }
+  }
+}
