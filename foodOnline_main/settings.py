e30143f9d2aaef52c5f3e5fe59baa294db7a731a
blacke4gle <ahmetdemirr0@gmail.com>
2023-05-24 15:31:37 +0300

    cart functionality

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'accounts',
     'vendor',
     'menu',
+    'marketplace',
 ]
 
 MIDDLEWARE = [
@@ -71,6 +72,8 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'accounts.context_processors.get_vendor',
                 'accounts.context_processors.get_google_api',
+                'marketplace.context_processors.get_cart_counter',
+                'marketplace.context_processors.get_cart_amounts',
             ],
         },
     },
---
+++ marketplace/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.marketplace, name='marketplace'),
+
+    path('<slug:vendor_slug>/', views.vendor_detail, name='vendor_detail'),
+
+    # ADD TO CART
+    path('add_to_cart/<int:food_id>/', views.add_to_cart, name='add_to_cart'),
+
+    # DECRASE CART
+    path('decrease_cart/<int:food_id>/',
+         views.decrease_cart, name='decrease_cart'),
+    # DELETE CART ITEM
+    path('delete_cart/<int:cart_id>/', views.delete_cart, name='delete_cart')
+
+
+]
