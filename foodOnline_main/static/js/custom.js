b0d7a7cf35c7b7bd3e87540a186a706d23c1fca1
blacke4gle <ahmetdemirr0@gmail.com>
2023-06-02 11:28:27 +0300

    dynamic tax module

--- foodOnline_main/static/js/custom.js
+++ foodOnline_main/static/js/custom.js
@@ -97,7 +97,7 @@ $(document).ready(function () {
           // subtotal, tax and grand total
           applyCartAmounts(
             response.cart_amount["subtotal"],
-            response.cart_amount["tax"],
+            response.cart_amount["tax_dict"],
             response.cart_amount["grand_total"]
           );
         }
@@ -124,7 +124,6 @@ $(document).ready(function () {
       type: "GET",
       url: url,
       success: function (response) {
-        console.log(response);
         if (response.status == "login_required") {
           swal(response.message, "", "info").then(function () {
             window.location = "/login";
@@ -138,7 +137,7 @@ $(document).ready(function () {
           // subtotal, tax and grand total
           applyCartAmounts(
             response.cart_amount["subtotal"],
-            response.cart_amount["tax"],
+            response.cart_amount["tax_dict"],
             response.cart_amount["grand_total"]
           );
 
@@ -162,7 +161,7 @@ $(document).ready(function () {
       type: "GET",
       url: url,
       success: function (response) {
-        console.log(response);
+        // console.log(response);
         if (response.status == "Failed") {
           swal(response.message, "", "error");
         } else {
@@ -172,7 +171,7 @@ $(document).ready(function () {
           // subtotal, tax and grand total
           applyCartAmounts(
             response.cart_amount["subtotal"],
-            response.cart_amount["tax"],
+            response.cart_amount["tax_dict"],
             response.cart_amount["grand_total"]
           );
 
@@ -200,11 +199,18 @@ $(document).ready(function () {
   }
 
   // apply cart amounts
-  function applyCartAmounts(subtotal, tax, grand_total) {
+  function applyCartAmounts(subtotal, tax_dict, grand_total) {
     if (window.location.pathname == "/cart/") {
       $("#subtotal").html(subtotal);
-      $("#tax").html(tax);
       $("#total").html(grand_total);
+      // console.log(tax_dict);
+      for (key1 in tax_dict) {
+        // console.log(tax_dict[key1]);
+        for (key2 in tax_dict[key1]) {
+          // console.log(tax_dict[key1][key2]);
+          $("#tax-" + key1).html(tax_dict[key1][key2]);
+        }
+      }
     }
   }
 
@@ -243,10 +249,28 @@ $(document).ready(function () {
         success: function (response) {
           if (response.status == "success") {
             if (response.is_closed == "Closed") {
-              html = '<tr id="hour-'+response.id+'"><td><b>'+response.day+'</b></td><td>Closed</td><td><a href="#" class="remove_hour" data-url="/vendor/opening-hours/remove/'+response.id+'/">Remove</a></td></tr>';
+              html =
+                '<tr id="hour-' +
+                response.id +
+                '"><td><b>' +
+                response.day +
+                '</b></td><td>Closed</td><td><a href="#" class="remove_hour" data-url="/vendor/opening-hours/remove/' +
+                response.id +
+                '/">Remove</a></td></tr>';
               // html = "<tr id='hour-"+response.id+"><td><b>"+response.day+"</b></td><td>Closed</td><td><a href='#' class='remove_hour' data-url='vendor/opening-hours/remove/"+response.id+"/>Remove</a></td></tr>";
             } else {
-              html ='<tr id="hour-'+response.id+'"><td><b>'+response.day +'</b></td><td>'+response.from_hour+' - '+response.to_hour+'</td><td><a href="#" class="remove_hour" data-url="/vendor/opening-hours/remove/'+response.id+'/">Remove</a></td></tr>';
+              html =
+                '<tr id="hour-' +
+                response.id +
+                '"><td><b>' +
+                response.day +
+                "</b></td><td>" +
+                response.from_hour +
+                " - " +
+                response.to_hour +
+                '</td><td><a href="#" class="remove_hour" data-url="/vendor/opening-hours/remove/' +
+                response.id +
+                '/">Remove</a></td></tr>';
               // html ='<tr id="hour-'+response.id+'"><td><b>'+response.day +'</b></td><td>'+response.from_hour+' - '+response.to_hour+'</td><td><a href="#" class="remove_hour" data-url="/vendor/opening-hours/remove/'+response.id+'"/>Remove</a></td></tr>';
             }
             $(".opening_hours").append(html);
