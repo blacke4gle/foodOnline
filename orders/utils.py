2b16bf3c217fb2234e81650a87312ed91d45f7b2
blacke4gle <ahmetdemirr0@gmail.com>
2023-06-10 10:23:55 +0300

    email template implementation

--- orders/utils.py
+++ orders/utils.py
@@ -1,4 +1,5 @@
 import datetime
+import simplejson as json
 
 
 def generate_order_number(pk):
@@ -6,3 +7,30 @@ def generate_order_number(pk):
         '%Y%m%d%H%M%S')  # 20230605085215 + pk
     order_number = current_datetime + str(pk)
     return order_number
+
+
+def order_total_by_vendor(order, vendor_id):
+    total_data = json.loads(order.total_data)
+    data = total_data.get(str(vendor_id))
+    subtotal = 0
+    tax = 0
+    tax_dict = {}
+
+    for key, val in data.items():
+        subtotal += float(key)
+        val = val.replace("'", '"')
+        val = json.loads(val)
+        tax_dict.update(val)
+
+        # calculate tax
+        # {'CGST': {'9.00': '6.03'}, 'SGST': {'7.00': '4.69'}}
+        for i in val:
+            for j in val[i]:
+                tax += float(val[i][j])
+    grand_total = float(subtotal) + float(tax)
+    context = {
+        'subtotal': subtotal,
+        'tax_dict': tax_dict,
+        'grand_total': grand_total,
+    }
+    return context
