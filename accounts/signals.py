fc5ae51e8eea8de0eb61a67546d8a6b2ab167ea6
blacke4gle <ahmetdemirr0@gmail.com>
2023-06-18 17:06:14 +0300

    database backup

--- accounts/signals.py
+++ accounts/signals.py
@@ -3,21 +3,21 @@ from django.dispatch import receiver
 from .models import User, UserProfile
 
 
-@receiver(post_save, sender=User)
-def post_save_create_profile_receiver(sender, instance, created, **kwargs):
-    print(created)
-    if created:
-        UserProfile.objects.create(user=instance)
-        print('User Profile is created')
-    else:
-        try:
-            profile = UserProfile.objects.get(user=instance)
-            profile.save()
-        except:
-            # Create the userProfile if no exist
-            UserProfile.objects.create(user=instance)
-            print('Profile was not exist, but I created one')
-        print('user is updated')
+# @receiver(post_save, sender=User)
+# def post_save_create_profile_receiver(sender, instance, created, **kwargs):
+#     print(created)
+#     if created:
+#         UserProfile.objects.create(user=instance)
+#         print('User Profile is created')
+#     else:
+#         try:
+#             profile = UserProfile.objects.get(user=instance)
+#             profile.save()
+#         except:
+#             # Create the userProfile if no exist
+#             UserProfile.objects.create(user=instance)
+#             print('Profile was not exist, but I created one')
+#         print('user is updated')
 
 
 @receiver(pre_save, sender=User)
